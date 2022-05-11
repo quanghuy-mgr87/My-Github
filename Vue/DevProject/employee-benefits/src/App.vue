0952f6fe2b75ef56b950dde9a54873098602dfcc
quanghuy-mgr87 <91078298+quanghuy-mgr87@users.noreply.github.com>
2022-05-11 13:21:52 +0700

    commit

---
+++ Vue/DevProject/employee-benefits/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <benefit-modal></benefit-modal>
+  </div>
+</template>
+
+<script>
+import BenefitModal from "./components/BenefitModal/BenefitModal.vue";
+export default {
+  name: "App",
+  components: {
+    BenefitModal,
+  },
+};
+</script>
---
+++ Vue/DevProject/employee-benefits/src/components/BenefitModal/BenefitModal.vue
@@ -0,0 +1,19 @@
+<template>
+  <div style="background: #ececec; padding: 30px">
+    <!-- <a-card title="Card title" :bordered="false" style="width: 300px">
+      <p>Card content</p>
+      <p>Card content</p>
+      <p>Card content</p>
+    </a-card> -->
+    <h1>ádf</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    components:{
+    }
+};
+</script>
+
+<style></style>
